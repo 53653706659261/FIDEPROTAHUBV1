468745b3cbeac03d7dc3e4360b02852c554d4289
53653706659261 <iij58245@gmail.com>
2024-12-03 11:19:34 -0300

    Update forcetp.lua

--- modules/forcetp.lua
+++ modules/forcetp.lua
@@ -1,1 +1,11 @@
+return function()
+    local plr = game.Players.LocalPlayer
+    local antBugItem = plr.Backpack:FindFirstChild("Antbug")
+    if antBugItem then
+        antBugItem:Destroy()
+        print("Antbug destruído com sucesso.")
+    else
+        print("Antbug não encontrado na Backpack.")
+    end
+end
 
